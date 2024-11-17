03c93449d5f2a46ef047ab3b1d9b82527b3b151c
Gainer552 <bszabada@hotmail.com>
2024-11-16 17:15:44 -0800

    Add files via upload

---
+++ archmedic.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+cat << "EOF"
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▓██▓▓▓▓▓▓███▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓█▓▓▓░░░▒▓▓▓████▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██▓▓▓░░░▒▓▓▓████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██▓▓▓░░░▒▓▓▓████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██▓▓▓░░░▓▓▓█████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██▓██▒▒░▓███████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█████▒▒▒▓███████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░▒▓█████████████████▒▒▒▓████████████████████▓░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░▓██▓▓▓▓▓▓███████████▒▒▒▓██████████████████████▒░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░█▓▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▓███████████████████████░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░▒█▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓███████████████████████░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░▒█▓▓▓▓▓▓▓▓▓▓▓█▓▓▓▓▓▓▓▓▓▓████████████████████████░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░█▓▓▓▓▓█████████████████████████████████████████░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░▓█████████████████████████████████████████████▒░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░▒██████████████████████████████████████████▓▒░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒█████████████████▒▒▒▒▒▒▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████████████████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████████████████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████████████████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████████████████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████████████████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██████████████▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▓▓▓▓▓▓▓▓▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+EOF
+echo
+
+# Description
+echo "Arch Medic: Your ultimate first-aid kit for Arch Linux! Arch Medic is a powerful yet easy-to-use program designed to 
+diagnose and fix common issues that can break Arch installations. Whether it’s a failed update, broken dependencies, 
+or misconfigured settings, Arch Medic swiftly restores your system to peak performance with a series of tried-and-true commands. 
+Built with beginners in mind, Arch Medic gives you peace of mind, helping you get back to what you do best—exploring the 
+freedom of Arch Linux! To exit press CTRL + C."
+echo
+sleep 15s
+echo "Warning: Arch Medic is provided as-is without any warranties. Use at your own risk. 
+The developer is not responsible for any data loss, system damage, or other issues resulting from the use of this program. 
+Always back up your data before making system changes."
+sleep 15s
+
+while true; do
+	# Options menu.
+	echo "Review a number of options for fixing common problems with Arch installations: "
+	echo
+
+	# Options to check logs.
+	echo "1) Check for missing dependencies."
+	echo "2) Check for errors in journalctl."
+	echo "3) Check for failed systemd units"
+	echo "4) Check for failed systemd services"	
+	echo "5) Check if mirrors are synced."
+
+	# Options to repair system.
+	echo "6) Fix partial upgrade."
+	echo "7) Remove a conflicting/corrupt package."
+	echo "8) Reinstall specified key package (linux, systemd, glibc etc.)"
+	echo "9) Reinstall core packages (systemd & glibc)."
+	echo "10) Downgrade a package"
+	echo "11) Download timeshift to create system restore points."
+	echo "12) Restore system to previous state."
+	echo
+
+	# Prompt user for selection.
+	read -p "Begin by making a selection: " usr_Input
+	echo
+
+	# Options
+	case $usr_Input in
+	# View Log Options:
+	1)sudo pacman -Qk
+	echo;;
+	2)journalctl -e
+	echo;;
+	3)sudo systemctl --unit=failed
+	echo;; 
+	4)sudo systemctl --state=failed
+	echo;;
+	5)sudo pacman -Syy
+	echo;;
+	# Repair Options:
+	6)	read -p "Would you like to upgrade Arch? (y/n): " upgrade
+		echo
+		if [[ "$upgrade" == "Y" || "$upgrade" == "y" ]]; then
+		sudo pacman -Syu
+		echo
+		echo "System upgraded!"
+		echo
+	else
+		echo "System not upgraded."
+		echo
+	fi;;
+	7)read -p "What package should be removed? " pkg_Name
+		sudo pacman -Rns $pkg_Name
+		echo
+		echo "Package removed."
+		echo;;
+	8)read -p "What package would you like to add? " pkg_Choice
+		sudo pacman -S $pkg_Choice
+		echo
+		echo "Package installed!"
+		echo;;
+	9)sudo pacman -S systemd glibc
+		echo;;
+	10) read -p "Which package would you like to downgrade? " $pkg_Dgrde
+		sudo downgrade $pkg_Dgrde;;
+	11)
+	if  command -v timeshift >/dev/null 2>&1 ; then
+		echo "Timeshift is already installed!"
+		echo
+	else
+		echo "Timeshift is not installed!"
+		echo
+		sudo pacman -S timeshift
+		echo
+		sleep 15s
+		sudo timeshift --create --comments "Manual snapshot" --tags Daily
+		echo
+	fi;;
+	12) read -p "Please specify the path of the restore point file: " system_img
+	if [ -f "$system_img" ]; then
+	sudo timeshift --restore $system_img
+	echo
+	echo "System restoration complete!"
+	fi;;
+	*) 	echo
+		echo "Please make a valid selection";;
+	esac
+done
